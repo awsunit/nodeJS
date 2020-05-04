c44cc16ef039fcfdaa0d5e50a33daa97853a5848
Todd Gilbert <awsunit@cs.washington.edu>
2020-05-03 22:56:59 -0700

    table creation - gui next

--- connect.js
+++ connect.js
@@ -1,61 +1,4 @@
-// // const mysql = require('mysql');
 
-// var config = 
-// {
-//     host: 'learningrph.database.windows.net',
-//     user: 'mesa',
-//     password: 'AntiMamaSlapping88',
-//     database: 'Hound',
-//     // port: 3306,
-//     // ssl: true
-// };
-
-// const conn = new mysql.createConnection(config);
-
-// conn.connect(
-//     function(err) {
-//         if (err) {
-//             console.log("DB unavailable, Error:");
-//             throw err;
-//         }
-//         else
-//         {
-//             console.log("connected");
-//             queryDatabase();
-//         }
-//     }
-// );
-
-// function queryDatabase(){
-//     conn.query('DROP TABLE IF EXISTS inventory;', function (err, results, fields) { 
-//          if (err) throw err; 
-//          console.log('Dropped inventory table if existed.');
-//      })
-//       conn.query('CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);', 
-//            function (err, results, fields) {
-//                if (err) throw err;
-//          console.log('Created inventory table.');
-//      })
-//     conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['banana', 150], 
-//            function (err, results, fields) {
-//                if (err) throw err;
-//          else console.log('Inserted ' + results.affectedRows + ' row(s).');
-//         })
-//     conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['orange', 154], 
-//            function (err, results, fields) {
-//                if (err) throw err;
-//          console.log('Inserted ' + results.affectedRows + ' row(s).');
-//         })
-//     conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['apple', 100], 
-//      function (err, results, fields) {
-//                if (err) throw err;
-//          console.log('Inserted ' + results.affectedRows + ' row(s).');
-//         })
-//     conn.end(function (err) { 
-//      if (err) throw err;
-//      else  console.log('Done.') 
-//      });
-// };
 const { Connection, Request } = require("tedious")
 
 const config = {
@@ -69,23 +12,48 @@ const config = {
     server: "learningrph.database.windows.net",
     options: {
         database: "Hound",
-        encrypt: true
+        encrypt: true,
+        trustServerCertificate: true,
+        requestTimeout: 5000
     }
+    
+    
 };
 
 const connection = new Connection(config)
 
-connection.on("connect", err => {
+connection.connect(
+    function(err) {
     if (err) {
-        console.error(err.message);
-    } else {
+        console.log("DB unavailable, Error:");
+        throw err;
+    }
+    else
+    {
+        console.log("connected");
         createDatabase();
+        // closeConnection();
     }
 });
 
 function createDatabase() {
     console.log("he's trying")
+
+    const request = new Request(
+        'CREATE TABLE creators( creator VARCHAR(255) NOT NULL, hashedpass VARCHAR(255) NOT NULL, PRIMARY KEY (creator));',
+        (err, rowCount) => {
+            if (err) {
+                console.log(err.message);
+            } else {
+                console.log('Created Table')
+            }
+        }
+    );
+    connection.execSql(request);
+    // return process.exit(0);
+
 }
+
 
 
 
